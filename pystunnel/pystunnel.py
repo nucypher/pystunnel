61d6df6dea33179732cac0f23ec7d2519b3ea289
Stefan H. Holek <stefan@epy.co.at>
2016-03-11 21:01:24 +0100

    Extract StunnelConfig, lose lazy, etc.

--- pystunnel/pystunnel.py
+++ pystunnel/pystunnel.py
@@ -8,11 +8,42 @@ import re
 import subprocess
 import getopt
 
-from lazy import lazy
+
+class StunnelConfig:
+    """Represent a stunnel configuration"""
+
+    _section_re = re.compile(r"\[(.*)\]")
+    _foreground_re = re.compile(r"foreground\s*=\s*(.*)")
+    _pid_file_re = re.compile(r"pid\s*=\s*(.*)")
+
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.pid_file = None
+        self.foreground = False
+        self._read_config()
+
+    def _read_config(self):
+        if self.config_file and os.path.isfile(self.config_file):
+            lines = []
+            with open(self.config_file, "rt") as f:
+                lines = f.readlines()
+            for line in lines:
+                line = line.strip()
+                match = self._section_re.match(line)
+                if match:
+                    return
+                match = self._pid_file_re.match(line)
+                if match:
+                    self.pid_file = match.group(1)
+                else:
+                    match = self._foreground_re.match(line)
+                    if match:
+                        if match.group(1) == "yes":
+                            self.foreground = True
 
 
-class Stunnel:
-    """Start and stop a stunnel instance given a config file
+class Stunnel(StunnelConfig):
+    """Start and stop a stunnel instance given a configuration file
 
     The config file must contain a pid = /path/to/pid-file line.
 
@@ -30,16 +61,21 @@ class Stunnel:
 
         rc = stunnel.stop()
         print("stunnel stopped with rc", rc)
+
+    Return Codes:
+        0 means OK, 1 or higher means error.
+
+        The start command can fail for a variety of reasons, the most common
+        being bad or missing configuration files. Inspecting the config_file,
+        pid_file, and foreground attributes may provide hints on the nature
+        of the error.
     """
 
-    _pid_file_re = re.compile(r"pid\s*=\s*(.*)")
-
     def __init__(self, config_file):
-        self.config_file = config_file
-        lazy.invalidate(self, 'pid_file')
+        StunnelConfig.__init__(self, config_file)
 
     def start(self):
-        if self.pid_file and self.check() == 1:
+        if self.pid_file and not self.foreground and self.check() == 1:
             return subprocess.call('stunnel "%s"' % self.config_file, shell=True);
         return 1
 
@@ -64,32 +100,27 @@ class Stunnel:
                         pass
         return pid
 
-    @lazy
-    def pid_file(self):
-        if self.config_file and os.path.isfile(self.config_file):
-            with open(self.config_file, "rt") as f:
-                lines = f.readlines()
-                for line in lines:
-                    match = self._pid_file_re.match(line.strip())
-                    if match:
-                        return match.group(1)
-        return ""
-
 
 class PyStunnel(Stunnel):
-    """Usage: pystunnel -c filename [command]
+    """Usage: pystunnel options [command]
 
-Start and stop a stunnel instance from the command line.
+Start and stop a stunnel instance from the command line
 
 Options:
-    -c filename, --stunnel-config=filename
+  -c config-file, --stunnel-config=config-file
+                      Use config-file to drive the stunnel instance.
+                      The stunnel configuration must specify a PID file.
 
-    The config file must contain a pid = /path/to/pid-file line.
+  -h, --help          Print this help message and exit.
+  -v, --version       Print the version string and exit.
 
 Commands:
-    start, stop, check, getpid
+  start               Start the stunnel instance.
+  stop                Stop the stunnel instance.
+  check               Check if stunnel is running.
+  getpid              Return PID of running stunnel instance.
 
-    If command is omitted, pystunnel enters an interactive shell.
+  If the command is omitted, pystunnel enters an interactive shell.
 """
 
     def __init__(self, args=None):
@@ -98,7 +129,7 @@ Commands:
 
     def parse_args(self, args):
         try:
-            options, args = getopt.gnu_getopt(args, "c:h", ("stunnel-config=", "help"))
+            options, args = getopt.gnu_getopt(args, "c:hv", ("stunnel-config=", "help", "version"))
         except getopt.GetoptError, e:
             print(e.msg)
             sys.exit(1)
@@ -109,15 +140,22 @@ Commands:
             elif name in ("-h", "--help"):
                 print(self.__doc__)
                 sys.exit(0)
+            elif name in ("-v", "--version"):
+                import pkg_resources
+                print("pystunnel", pkg_resources.get_distribution("pystunnel").version)
+                sys.exit(0)
 
         if not self.config_file:
             print("option -c is required")
             sys.exit(1)
         elif not os.path.isfile(self.config_file):
-            print("file not found:", repr(self.config_file))
+            print("no such file:", self.config_file)
             sys.exit(1)
         elif not self.pid_file:
-            print("invalid configuration file:", repr(self.config_file))
+            print("must set pid = /path/to/pid-file in", self.config_file)
+            sys.exit(1)
+        elif self.foreground:
+            print("must set foreground = no in", self.config_file)
             sys.exit(1)
 
         return args
@@ -129,7 +167,7 @@ Commands:
             if rc == 0:
                 print("started")
             elif self.check() == 0:
-                print("already started")
+                print("already started (%s)" % self.getpid())
             else:
                 print("not started!")
         elif command == "stop":
@@ -143,7 +181,7 @@ Commands:
         elif command == "check":
             rc = self.check()
             if rc == 0:
-                print("running")
+                print("running (%s)" % self.getpid())
             else:
                 print("not running")
         elif command == "getpid":
